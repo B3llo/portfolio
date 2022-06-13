47d997d2864f34982fd58e9d5ed4bd1cdc32b22a
Gabriel F. Bello <gfbbello@outlook.com>
2022-06-12 22:27:58 -0300

    Navbar Bottom Border on Scroll

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -3,13 +3,37 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  /**
+   * Flag to show/hide the navbar bottom border
+   */
+  public showNavbarBorder: boolean = false;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
-  }
+    // Necessary variable to avoid "this" conflict
+    let isScrolling = false;
 
+    window.onscroll = function () {
+      if (window.scrollY > 30) {
+        isScrolling = true;
+      } else {
+        isScrolling = false;
+      }
+
+      isScrollingEvent(isScrolling);
+    };
+
+    const isScrollingEvent = (isScrolling: boolean) => {
+      if (isScrolling) {
+        this.showNavbarBorder = true;
+        console.log('showNavbarBorder: ', this.showNavbarBorder);
+      } else {
+        this.showNavbarBorder = false;
+      }
+    };
+  }
 }
