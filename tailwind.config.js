17cd4089ce42acd3e77abaeceda2ba811bc7b69d
Gabriel F. Bello <gfbbello@outlook.com>
2022-06-15 00:03:00 -0300

    About Me Block

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,17 @@ module.exports = {
         poppins: ["Poppins", "sans-serif"],
       },
     },
+    borderRadius: {
+      none: "0",
+      sm: "0.125rem",
+      DEFAULT: "0.25rem",
+      DEFAULT: "4px",
+      md: "0.375rem",
+      lg: "0.5rem",
+      full: "9999px",
+      large: "12px",
+      xlarge: "80px",
+    },
   },
   plugins: [],
 };
